9641e34b3e4f36fc49e2844fcdeb7231e6b7a118
ffiuza2 <ffiuzasantos@gmail.com>
2025-02-13 19:55:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Sorteador de numeros - sorteia aleatoriamente numeros
+// quantidade de numeros iformada pelo usuario. 
+// inicio e fim dos numeros informado pelo usuario
+// botao de sortear
+// botão de reiniciar
+
