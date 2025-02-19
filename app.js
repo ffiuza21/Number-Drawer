ba523dd074221b488a7c449c631d7a4fe1bae6bc
ffiuza2 <ffiuzasantos@gmail.com>
2025-02-19 18:30:45 -0300

    Finishing Number Drawer Game

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function draw () {
+function draw() {
     let quantity = parseInt(document.getElementById('quantity').value);
     let inicial = parseInt(document.getElementById('inicial').value);
     let limit = parseInt(document.getElementById('limit').value);
@@ -19,8 +19,29 @@ function draw () {
 
     let result = document.getElementById('result');
     result.innerHTML = `<label class="texto__paragrafo">Drawn Numbers: ${drawn}</label>`;
+
+    enableRestart();
 }
 
 function getRamdomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function enableRestart() {
+    let restartButton = document.getElementById("btn-restart");
+    if (restartButton.classList.contains('container__disabled-button')) {
+        restartButton.classList.remove('container__disabled-button');
+        restartButton.classList.add('container__botao');
+    } else {
+        restartButton.classList.remove('container__botao');
+        restartButton.classList.add('container__disabled-button');
+    }
+}
+
+function restart() {
+    document.getElementById('quantity').value = '';
+    document.getElementById('inicial').value ='';
+    document.getElementById('limit').value ='';
+    document.getElementById('result').innerHTML = 'Drawn Numbers: none';
+    enableRestart();
 }
